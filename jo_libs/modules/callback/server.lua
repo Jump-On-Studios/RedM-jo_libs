709235e05cb1cb3d6238d79538b0d824288fd820
KadDarem <cressent.h@orange.fr>
2024-04-25 17:13:44 +0200

    update callback

--- jo_libs/modules/callback/server.lua
+++ jo_libs/modules/callback/server.lua
@@ -1,19 +1,19 @@
-ServerCallbacks = {}
+local serverCallbacks = {}
 
-function RegisterServerCallback(name, cb)
-  ServerCallbacks[name] = {
+function jo.registerServerCallback(name, cb)
+  serverCallbacks[name] = {
     cb = cb,
     resource = GetCurrentResourceName()
   }
 end
 
-function FireServerCallback(name, source, cb, ...)
-  cb(ServerCallbacks[name].cb(source, ...))
+local function FireServerCallback(name, source, cb, ...)
+  cb(serverCallbacks[name].cb(source, ...))
 end
 
 RegisterServerEvent('jo_libs:triggerServerCallback', function(name, requestId,fromRessource, ...)
   local source = source
-  if not ServerCallbacks[name] then return end
+  if not serverCallbacks[name] then return end
 
   FireServerCallback(name, source, function(...)
     TriggerClientEvent('jo_libs:serverCallback', source, requestId,fromRessource, ...)
