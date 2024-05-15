5f4fa836e790d3bc668f17ab8760fd50de6b311a
KadDarem <cressent.h@orange.fr>
2024-05-15 09:29:57 +0200

    feat(blip): Add color

--- jo_libs/modules/blip/client.lua
+++ jo_libs/modules/blip/client.lua
@@ -12,13 +12,17 @@ end)
 ---@param name string name of the blip
 ---@param sprite string sprite of the blip
 ---@param blipHash? integer the type of blip
+---@param color? string the blip color
 ---@return integer blip the blip ID
-function jo.blip.create(location,name,sprite, blipHash)
+function jo.blip.create(location,name,sprite, blipHash, color)
   if not blipHash then blipHash = 1664425300 end
   if type(sprite) == "string" then sprite = joaat(sprite) end
   local blip = BlipAddForCoords(blipHash,location.x, location.y, location.z)
   SetBlipSprite(blip, sprite)
   SetBlipName(blip, name)
+  if color then
+    BlipAddModifier(blip, GetHashFromString(color))
+  end
   Blips[#Blips+1] = blip
   return blip
 end
