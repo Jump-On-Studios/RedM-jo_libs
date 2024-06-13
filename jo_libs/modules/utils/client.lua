dd81ce57dc028eafde6b0fb02bb5e9fe256ace95
Elzetia <cy.buniet@gmail.com>
2024-06-13 21:01:33 +0200

    added update level function

--- jo_libs/modules/utils/client.lua
+++ jo_libs/modules/utils/client.lua
@@ -71,4 +71,19 @@ function jo.utils.waiter(cb,maxDuration,loopTimer)
 	return true
 end
 
+
+---@param level integer
+---@param xp integer 
+---@param xpRequired integer 
+function jo.utils.updateLevelToast(level, xp, xpRequired)
+    local container = DatabindingAddDataContainerFromPath("", "mp_rank_bar")
+    DatabindingAddDataString(container, "rank_text", tostring(level))
+    DatabindingAddDataString(container, "rank_header_text", xp .. "/" .. xpRequired)
+    DatabindingAddDataInt(container, "rank_header_text_alpha", 100)
+    DatabindingAddDataInt(container, "xp_bar_minimum", 0)
+    DatabindingAddDataInt(container, "xp_bar_maximum", xpRequired)
+    DatabindingAddDataInt(container, "xp_bar_value", xp)
+end
+
+
 return jo.utils
