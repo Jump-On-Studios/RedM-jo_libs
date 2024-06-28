ec23de38112bed18e9b568fcd98cb4aa55f488ac
KadDarem <cressent.h@orange.fr>
2024-06-28 11:25:49 +0200

    Update (callback):  Add `jo.callback.triggerClient()`

--- jo_libs/modules/callback/client.lua
+++ jo_libs/modules/callback/client.lua
@@ -1,5 +1,6 @@
 local currentRequestId = 0
 local serverCallbacks = {}
+local clientCallbacks = {}
 
 jo.callback = {}
 
@@ -13,6 +14,8 @@ function jo.callback.triggerServer(name,cb, ...)
   TriggerServerEvent('jo_libs:triggerServerCallback', name, currentRequestId,fromRessource, ...)
   currentRequestId = currentRequestId < 65535 and currentRequestId + 1 or 0
 end
+
+--deprecated function
 jo.triggerServerCallback = jo.callback.triggerServer
 
 RegisterNetEvent('jo_libs:serverCallback', function(requestId,fromRessource, ...)
@@ -23,6 +26,27 @@ RegisterNetEvent('jo_libs:serverCallback', function(requestId,fromRessource, ...
   else
     print('[^1ERROR^7] Server Callback with requestId ^5'.. requestId ..'^7 Was Called by ^5'.. fromRessource .. '^7 but does not exist.')
   end
+end)
+
+---@param name string the name of the event
+---@param cb function
+function jo.callback.register(name, cb)
+  clientCallbacks[name] = {
+    cb = cb,
+    resource = GetCurrentResourceName()
+  }
+end
+
+local function FireClientCallback(name, cb, ...)
+  cb(clientCallbacks[name].cb(...))
+end
+
+RegisterNetEvent('jo_libs:triggerClientCallback', function(name, requestId,fromRessource, ...)
+  if not clientCallbacks[name] then return end
+
+  FireClientCallback(name, function(...)
+    TriggerServerEvent('jo_libs:clientCallback', requestId,fromRessource, ...)
+  end, ...)
 end)
 
 return jo.callback
