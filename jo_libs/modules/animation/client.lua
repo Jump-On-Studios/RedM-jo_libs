e577f7a18155ee4ca4a4482bfd411de17a280477
KadDarem <cressent.h@orange.fr>
2024-07-12 09:36:48 +0200

    Update animation

--- jo_libs/modules/animation/client.lua
+++ jo_libs/modules/animation/client.lua
@@ -12,9 +12,14 @@ function jo.animation.play(ped,dict,name,duration,flag,offset)
   if not duration then duration = -1 end
   if not flag then flag = 0 end
   if not offset then offset = 0.0 end
-  loadAnimDict(dict,true)
-  TaskPlayAnim(ped, dict, name, 4.0, -4.0, duration, flag, offset, false, false, false)
-  RemoveAnimDict(dict)
+  CreateThread(function()
+    loadAnimDict(dict,true)
+    TaskPlayAnim(ped, dict, name, 4.0, -4.0, duration, flag, offset, false, false, false)
+    Wait(timer)
+    RemoveAnimDict(dict)
+    waiter:resolve(true)
+  end)
+  return waiter
 end
 
 return jo.animation
