31bc4bb4bd298a6778a9bc4885b38b3de82ddf91
KadDarem <cressent.h@orange.fr>
2024-08-07 13:36:29 +0200

    New library: screen

---
+++ jo_libs/modules/screen/client.lua
@@ -0,0 +1,22 @@
+jo.screen = {}
+
+---@param duration? integer duration of the fade in ms (default: 500)
+---@param needWait? boolean wait the end of the fade (default:false)
+function jo.screen.fadeIn(duration,needWait)
+  DoScreenFadeIn(duration or 500)
+  while needWait and IsScreenFadingOut() do
+    Wait(0)
+  end
+end
+
+---@param duration? integer duration of the fade in ms (default: 500)
+---@param needWait? boolean wait the end of the fade (default:true)
+function jo.screen.fadeOut(duration,needWait)
+  if (needWait == nil) then needWait = true end
+  DoScreenFadeOut(duration or 500)
+  while needWait and IsScreenFadingOut() do
+    Wait(0)
+  end
+end
+
+return jo.screen
