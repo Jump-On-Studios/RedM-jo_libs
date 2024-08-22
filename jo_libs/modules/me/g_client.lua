4c8eb70ab86c3bd5a639beaf83fba5f48f09d612
KadDarem <cressent.h@orange.fr>
2024-08-22 18:31:35 +0200

    Update (me) : refactor to use global file

---
+++ jo_libs/modules/me/g_client.lua
@@ -0,0 +1,38 @@
+jo.me = PlayerPedId()
+jo.meCoords = GetEntityCoords(me)
+jo.mePlayerId = PlayerId()
+jo.meServerId = GetPlayerServerId(jo.mePlayerId)
+jo.meIsMale =  IsPedMale(PlayerPedId())
+local timer = 1000
+local timeout
+
+if not IsModuleLoaded('timeout') then
+  jo.require('timeout')
+end
+
+local function updateMe()
+  jo.forceUpdateMe()
+  TriggerEvent("jo_me:updateMe",jo.me,jo.meCoords,jo.mePlayerId,jo.meServerId,jo.meIsMale)
+end
+timeout = jo.timeout.loop(timer,updateMe)
+
+---@param value integer the new interval to update me values
+function jo.updateMeTimer(value)
+  timer = value
+  if timeout then
+    timeout:clear()
+  end
+  if timer then
+    timeout = jo.timeout.loop(timer,updateMe)
+  end
+end
+
+function jo.forceUpdateMe()
+  jo.me = PlayerPedId()
+  jo.meCoords = GetEntityCoords(jo.me)
+  jo.mePlayerId = PlayerId()
+  jo.meServerId = GetPlayerServerId(jo.mePlayerId)
+  jo.meIsMale =  IsPedMale(jo.me)
+end
+
+exports('jo_me_forceUpdateMe', jo.forceUpdateMe)
