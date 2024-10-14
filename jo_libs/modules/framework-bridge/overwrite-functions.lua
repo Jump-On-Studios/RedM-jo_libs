bda3884b84f17f4fb3be16083b6be9bf8b399e71
KadDarem <cressent.h@orange.fr>
2024-10-14 16:11:13 +0200

    Update overwrite-functions.lua

--- jo_libs/modules/framework-bridge/overwrite-functions.lua
+++ jo_libs/modules/framework-bridge/overwrite-functions.lua
@@ -3,3 +3,27 @@
 -------------
 OWFramework = {}
 OWFramework.User = {}
+
+-------------
+-- SERVER SIDE
+-------------
+if IsDuplicityVersion() then
+
+  function OWFramework.get()
+    return "RedEM2023"
+  end
+
+  function OWFramework.initFramework(self)
+    bprint('fd_core detected')
+    self.core= exports["fd_core"]:FDCore()
+    TriggerEvent("fd_inventory:getData",function(call)
+      self.inv = call
+    end)
+  end
+
+  function OWFramework.openInventory(source, invName, name, invConfig)
+    TriggerClientEvent("fd_inventory:OpenStash", source, invName, invConfig.maxWeight)
+  end
+
+end
+OWFramework.User = {}
