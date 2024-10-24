f5f6dc163c880331206afd7a7eeb38e6d25d06aa
AlexbavGamer <alexsandrebav@outlook.com>
2024-10-24 20:15:09 -0300

    correção do Framework.updateUserClothes

--- modules/framework-bridge/server.lua
+++ modules/framework-bridge/server.lua
@@ -1095,7 +1095,7 @@ function FrameworkClass:updateUserClothes(source, _clothes, value)
   end
   local clothes = revertClothesKeys(_clothes)
   if OWFramework.updateUserClothes then
-    return OWFramework.updateUserClothes(source, category, value)
+    return OWFramework.updateUserClothes(source, _clothes, value)
   end
   if self:is('VORP') then
     local newClothes = {}
