53f3f9310fe4b9a9102ea768673dcc5852fcb6f7
KadDarem <cressent.h@orange.fr>
2024-11-14 19:48:04 +0100

    Update (math): added math.round()

--- jo_libs/modules/math/shared.lua
+++ jo_libs/modules/math/shared.lua
@@ -2,5 +2,10 @@ function math.lerp(a, b, t)
   return a + (b - a) * t
 end
 
+function math.round(number, precision)
+  if not precision or precision <= 0 then return math.floor(number) end
+  return math.floor(number * math.pow(10, precision)) / math.pow(10, precision)
+end
+
 jo.math = {}
 return jo.math
