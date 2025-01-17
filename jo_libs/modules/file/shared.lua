eb4ae6d94cc2de1150a7c36007a1caf4b99986b6
KadDarem <cressent.h@orange.fr>
2025-01-17 11:40:56 +0100

    Update (file): Allow to load file from another resource `@resource.folder.file`

--- jo_libs/modules/file/shared.lua
+++ jo_libs/modules/file/shared.lua
@@ -1,21 +1,40 @@
 jo.file = {}
 
 function jo.file.load(modname)
-  if type(modname) ~= 'string' then return end
-  local modpath = modname:gsub('%.', '/')
+  if type(modname) ~= "string" then return end
 
-  local file = LoadResourceFile('jo_libs', ('modules/%s.lua'):format(modpath))
+  local modpath = modname:gsub("%.", "/")
+  local resource = ""
+
+  if modpath:sub(1, 1) == "@" then
+    function splitString(input, delimiter)
+      local result = {}
+      for match in (input .. delimiter):gmatch("(.-)" .. delimiter) do
+        table.insert(result, match)
+      end
+      return result
+    end
+    for match in (modpath .. "/"):gmatch("(.-)" .. "/") do
+      resource = match:sub(2)
+      break
+    end
+    modpath = modpath:sub(resource:len() + 3)
+  else
+    resource = "jo_libs"
+    modpath = "modules/" .. modpath
+  end
+
+  local file = LoadResourceFile(resource, ("%s.lua"):format(modpath))
 
   if file then
-    local fn, err = load(file, ('@@jo_libs/modules/%s.lua'):format(modpath))
+    local fn, err = load(file, ("@@%s/%s.lua"):format(resource, modpath))
 
     if not fn or err then
-      return error(('\n^1Error loading file (%s): %s^0'):format(modpath, err), 3)
+      return error(("\n^1Error loading file (%s): %s^0"):format(modname, err), 3)
     end
 
     pcall(fn)
   else
-    eprint('Impossible to load: ' .. modname)
+    eprint("Impossible to load: " .. modname)
   end
 end
-
