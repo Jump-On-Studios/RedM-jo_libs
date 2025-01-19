2df4740a7304fd30e6c36775838362217389ee57
AlexbavGamer <Alexsandrebav@outlook.com>
2025-01-19 01:08:12 -0300

    fix: correct parameter order in updateUserClothes function

--- modules/framework-bridge/server.lua
+++ modules/framework-bridge/server.lua
@@ -2761,7 +2761,7 @@ function FrameworkClass:updateUserClothes(source, _clothes, value)
   end
   local clothes = revertClothes(_clothes)
   if OWFramework.updateUserClothes then
-    return OWFramework.updateUserClothes(source, category, value)
+    return OWFramework.updateUserClothes(source, _clothes, value)
   end
   if self:is("VORP") then
     local newClothes = {}
