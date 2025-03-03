cbf20155e29e9f00c2a2ae85c85b25e1e1a48e80
Brice Nuzzo <brice.nuzzo@jumpon-studios.com>
2025-03-03 18:10:11 +0100

    day1

---
+++ source-repositories/RedM-Prompt/src/stores/group.js
@@ -0,0 +1,30 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const useGroupStore = defineStore('group', () => {
+  const title = ref("Stable Shop")
+  const position =ref("bottom-left")
+  const prompts = ref([
+      {
+        label:"Press Prompt",
+        keyboardKeys:["E"],
+      },
+      {
+        label:"Multi press Prompt",
+        keyboardKeys:["F","A"],
+      },
+      {
+        label:"Hold Prompt",
+        keyboardKeys:["SPACE"],
+        holdTime:1000
+      }
+    ]
+  )
+
+  // const doubleCount = computed(() => count.value * 2)
+  // function increment() {
+  //   count.value++
+  // }
+
+  return { title, prompts,position }
+})
