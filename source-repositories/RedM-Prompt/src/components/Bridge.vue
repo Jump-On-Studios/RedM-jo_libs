00782d0ac163fb77568abfa11e7426b8990a3abd
Brice Nuzzo <brice.nuzzo@jumpon-studios.com>
2025-03-04 17:50:27 +0100

    setup build config

--- source-repositories/RedM-Prompt/src/components/Bridge.vue
+++ source-repositories/RedM-Prompt/src/components/Bridge.vue
@@ -4,13 +4,17 @@ import { useGroupStore } from '@/stores/group'
 const groupStore = useGroupStore()
 
 window.addEventListener('message', (event) => {
+  console.log(event)
   const { type, data } = event.data
 
   switch (type) {
     case 'updateGroup':
       groupStore.updateGroup(data)
       break
-    case 'yyyy':
+    case 'keyPressed':
+      // todo
+      break
+    case 'keyReleased':
       // todo
       break
   }
--- source-repositories/RedM-Prompt/src/dev.js
+++ source-repositories/RedM-Prompt/src/dev.js
@@ -1,5 +1,3 @@
-export default {}
-
 if (import.meta.env.DEV) {
   const SendNUIMessage = window.postMessage
 
