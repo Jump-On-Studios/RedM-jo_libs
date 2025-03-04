cefa2de9e4bce0b2836a4e15418a42091ac2fd7c
Brice Nuzzo <brice.nuzzo@jumpon-studios.com>
2025-03-04 14:39:11 +0100

    refactor keyboard key to its own component

--- source-repositories/RedM-Prompt/src/components/Prompt.vue
+++ source-repositories/RedM-Prompt/src/components/Prompt.vue
@@ -1,4 +1,5 @@
 <script setup>
+import KeyboardKey from './KeyboardKey.vue'
 const props = defineProps({
   prompt: Object,
 })
@@ -10,14 +11,12 @@ const props = defineProps({
       {{ props.prompt.label }}
     </div>
     <div id="keyboardKeys">
-      <div
+      <KeyboardKey
         v-for="(keyboardKey, index) in props.prompt.keyboardKeys"
         :key="index"
-        class="keyboardKey bebas"
-        :class="{ holdable: props.prompt.holdTime }"
-      >
-        {{ keyboardKey }}
-      </div>
+        :holdTime="props.prompt.holdTime"
+        :kkey="keyboardKey"
+      />
     </div>
   </div>
 </template>
@@ -40,33 +39,6 @@ const props = defineProps({
     display: flex;
     flex-direction: row;
     align-items: center;
-    .keyboardKey {
-      position: relative;
-      background: #fff;
-      color: #000;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      font-size: 0.75rem;
-      min-height: 30px;
-      min-width: 30px;
-      padding: 0 12px;
-      &:not(:first-child) {
-        margin-left: 5px;
-      }
-      &.holdable {
-        outline: 4px solid rgba($color: #000000, $alpha: 0.5);
-
-        &::after {
-          content: '';
-          width: calc(100% + 15px);
-          height: 44px;
-          position: absolute;
-          background: rgba($color: #fff, $alpha: 0.3);
-          z-index: -1;
-        }
-      }
-    }
   }
 }
 </style>
