6f7b9dfd1ae685ad92bcb8cbbeaf4115d58f6d19
KadDarem <cressent.h@orange.fr>
2025-03-13 17:28:01 +0100

    Update (NUI): Added `jo.nui.focus()`

--- jo_libs/modules/nui/client.lua
+++ jo_libs/modules/nui/client.lua
@@ -1,18 +1,26 @@
 jo.nui = {}
 local nuiLoaded = {}
 
-function jo.nui.load(name, url)
+function jo.nui.load(uiName, url)
   if GetResourceMetadata(GetCurrentResourceName(), "ui_page") ~= "nui://jo_libs/nui/index.html" then
     return eprint('You have to use "nui://jo_libs/nui/index.html" as your main ui_page in the fxmanifest.lua')
   end
   if (url:sub(1, 1) == "@") then url = url:sub(2) end
   if (not url:find("://")) then url = "nui://" .. url end
-  if nuiLoaded[url] then return eprint("This nui is already loaded:", url) end
+  if nuiLoaded[uiName] then return eprint("This nui is already loaded:", uiName) end
 
-  nuiLoaded[url] = true
+  nuiLoaded[uiName] = true
   SendNUIMessage({
-    action = "loadNUI",
+    action = "jo_nui_loadNUI",
     url = url,
-    name = name
+    uiName = uiName,
+  })
+end
+
+function jo.nui.focus(uiName)
+  if not nuiLoaded[uiName] then return eprint("This nui is not loaded:", uiName) end
+  SendNUIMessage({
+    action = "jo_nui_focus",
+    uiName = uiName
   })
 end
