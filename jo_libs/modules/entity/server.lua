71f5b88d180579f78b1a64f758dc17f245ecc377
Brice Nuzzo JO <brice.nuzzo@jumpon-studios.com>
2025-04-02 20:24:56 +0200

    [entity] update doc

--- jo_libs/modules/entity/server.lua
+++ jo_libs/modules/entity/server.lua
@@ -1,6 +1,7 @@
 jo.entity = {}
 
----@param entity integer
+--- Delete an entity if it exists
+---@param entity integer (The entity ID to delete)
 function jo.entity.delete(entity)
   if not DoesEntityExist(entity) then return end
   DeleteEntity(entity)
