5674379ad26c989d021f40210e33b606b01cebbe
Brice Nuzzo JO <brice.nuzzo@jumpon-studios.com>
2025-04-03 10:44:46 +0200

    [input] update doc

--- jo_libs/modules/input/client.lua
+++ jo_libs/modules/input/client.lua
@@ -1,23 +1,24 @@
 jo.input = {}
 
----@param label string The Text above the typing field in the black square
----@param placeholder string An Example Text, what it should say in the typing field
----@param maxStringLenght? integer (optional) Maximum String Lenght
----@return string
-function jo.input.native(label, placeholder, maxStringLenght)
-    AddTextEntry('FMMC_KEY_TIP1', label)
-    DisplayOnscreenKeyboard(0, "FMMC_KEY_TIP1", "", placeholder, "", "", "", maxStringLenght or 60)
+--- A function to open the native input
+---@param label string (The text above the typing field in the black square)
+---@param placeholder string (An Example Text, what it should say in the typing field)
+---@param maxStringLength? integer (Maximum String length)
+---@return string (Return the text from the input)
+function jo.input.native(label, placeholder, maxStringLength)
+  AddTextEntry("FMMC_KEY_TIP1", label)
+  DisplayOnscreenKeyboard(0, "FMMC_KEY_TIP1", "", placeholder, "", "", "", maxStringLength or 60)
 
-    while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do
-      Wait(0)
-    end
-        
-    if UpdateOnscreenKeyboard() ~= 2 then
-      local result = GetOnscreenKeyboardResult() --Gets the result of the typing
-      Wait(100)
-      return result or ''
-    else
-      Wait(100)
-      return ''
-    end
+  while UpdateOnscreenKeyboard() ~= 1 and UpdateOnscreenKeyboard() ~= 2 do
+    Wait(0)
+  end
+
+  if UpdateOnscreenKeyboard() ~= 2 then
+    local result = GetOnscreenKeyboardResult() --Gets the result of the typing
+    Wait(100)
+    return result or ""
+  else
+    Wait(100)
+    return ""
+  end
 end
