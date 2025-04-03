f2d50c7875674e08fee2302a4a9d130a7b3d5377
Brice Nuzzo JO <brice.nuzzo@jumpon-studios.com>
2025-04-03 10:32:14 +0200

    [game-events] add doc

--- jo_libs/modules/game-events/client.lua
+++ jo_libs/modules/game-events/client.lua
@@ -2,7 +2,10 @@ jo.gameEvents = {}
 
 jo.gameEvents.eventListened = {}
 
+--- Register a listener for a specific game event
+---@param eventName string (The name of the event to listen for, or "all" to listen for all events)
+---@param callback function (The function that will be called when the event is triggered)
 function jo.gameEvents.listen(eventName, callback)
-    TriggerEvent('jo_libs:gameEvents:register', eventName)
-    AddEventHandler('jo_libs:gameEvents:' .. eventName, callback)
+    TriggerEvent("jo_libs:gameEvents:register", eventName)
+    AddEventHandler("jo_libs:gameEvents:" .. eventName, callback)
 end
