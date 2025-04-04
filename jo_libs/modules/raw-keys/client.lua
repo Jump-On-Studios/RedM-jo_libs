598a4006cd5907f3d2a37432d1d4b94e5dad088c
Brice Nuzzo JO <brice.nuzzo@jumpon-studios.com>
2025-04-04 16:19:01 +0200

    [raw-keys] update doc

--- jo_libs/modules/raw-keys/client.lua
+++ jo_libs/modules/raw-keys/client.lua
@@ -1,7 +1,10 @@
 jo.rawKeys = {}
 
 local events = {}
--- Function to listen for a key press
+
+--- Registers a listener for a specific key. When the key is pressed or released, the provided callback function is executed with a boolean value indicating the event state (true for pressed, false for released).
+--- @param key string (The identifier of the key to listen for. This should correspond to one of the keys defined in the [keyboard mappings](#keyboard-keys-mapping) (e.g., "A", "B", "F1", etc.).)
+--- @param callback function (The function to be executed when the key event occurs. It receives one parameter: <br> _boolean_ — `true` when the key is pressed, `false` when it is released.)
 function jo.rawKeys.listen(key, callback)
     TriggerEvent("jo_libs:rawKeys:register", key)
     events[key] = events[key] or {}
@@ -9,6 +12,8 @@ function jo.rawKeys.listen(key, callback)
     table.insert(events[key], event)
 end
 
+--- Removes the listener associated with the specified key. Use this function to stop listening for events on a key when it is no longer needed.
+--- @param key string (The identifier of the key for which the listener should be removed.)
 function jo.rawKeys.remove(key)
     if not events[key] then return end
     TriggerEvent("jo_libs:rawKeys:remove", key)
