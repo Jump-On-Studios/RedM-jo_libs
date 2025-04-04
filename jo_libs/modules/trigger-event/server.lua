7bfb1a128ebbc6c429af5fecd7513e305b9bf6fc
Brice Nuzzo JO <brice.nuzzo@jumpon-studios.com>
2025-04-04 15:10:07 +0200

    [trigger-event] add doc

--- jo_libs/modules/trigger-event/server.lua
+++ jo_libs/modules/trigger-event/server.lua
@@ -1,5 +1,9 @@
 jo.triggerEvent = {}
 
+--- Trigger an event on the server side
+---@param source integer (The source player ID to send the event to)
+---@param event string (The name of the event to trigger)
+---@param ... any (Additional parameters to pass to the event)
 function jo.triggerEvent.server(source, event, ...)
   TriggerClientEvent("jo_libs:trigger-event:client:receive", source, event, ...)
 end
