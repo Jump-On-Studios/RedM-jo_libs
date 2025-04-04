491b64ff866eef0564614439f509197def10e7ce
Brice Nuzzo JO <brice.nuzzo@jumpon-studios.com>
2025-04-04 15:41:16 +0200

    [utils] update doc

--- jo_libs/modules/utils/client.lua
+++ jo_libs/modules/utils/client.lua
@@ -1,8 +1,9 @@
 jo.utils = {}
 
----@param name string
----@param waiter? boolean need wait
-function jo.utils.loadGameData(name,waiter)
+--- A function to load a game file
+---@param name string|integer (The name of the file <br> Compatible with animation dictionaries, models (hashed or string), and texture dictionaries)
+---@param waiter? boolean (If the function has to wait after the loading to be completed)
+function jo.utils.loadGameData(name, waiter)
 	local model = (type(name) == "string") and joaat(name) or name
 	if IsModelValid(model) then
 		if not HasModelLoaded(model) then
@@ -20,7 +21,7 @@ function jo.utils.loadGameData(name,waiter)
 				Wait(0)
 			end
 		end
-    return
+		return
 	end
 	if DoesStreamedTextureDictExist(name) then
 		if not HasStreamedTextureDictLoaded(name) then
@@ -33,7 +34,8 @@ function jo.utils.loadGameData(name,waiter)
 	end
 end
 
----@param name string
+--- A function to release a game file
+---@param name string|integer (The name of the file)
 function jo.utils.releaseGameData(name)
 	local model = (type(name) == "string") and joaat(name) or name
 	if IsModelValid(model) then
@@ -56,12 +58,13 @@ function jo.utils.releaseGameData(name)
 	end
 end
 
-
----@param cb function the tester
----@param maxDuration integer (default: 1000)
----@param loopTimer integer (default: 0)
-function jo.utils.waiter(cb,maxDuration,loopTimer)
-	local endTimer = GetGameTimer()+ (maxDuration or 1000)
+--- A function to wait after a satisfying condition with a waiting duration
+---@param cb function (If the function returns `true` a new timer value is waited)
+---@param maxDuration? integer (The maximum duration the function will wait <br> default: 1000)
+---@param loopTimer? integer (The delay between tests of cb function in ms <br> default: 0)
+---@return boolean (Returns `true` if the function is satisfied, `false` if the waiter was killed by the maxDuration value)
+function jo.utils.waiter(cb, maxDuration, loopTimer)
+	local endTimer = GetGameTimer() + (maxDuration or 1000)
 	while cb() do
 		Wait(loopTimer or 0)
 		if endTimer < GetGameTimer() then
