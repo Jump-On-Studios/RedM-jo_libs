620cb07b5d5a12230eb39189962fb1d5aa62020f
KadDarem <cressent.h@orange.fr>
2025-04-11 17:05:53 +0200

    Removed debug prints

--- jo_libs/modules/framework-bridge/g_server.lua
+++ jo_libs/modules/framework-bridge/g_server.lua
@@ -7,9 +7,9 @@ RegisterNetEvent("jo_libs:server:applySkinAndClothes", function(ped, skin, cloth
   local source = source
 
   if jo.framework:is("VORP") then
-    if table.count(jo.framework:getUserIdentifiers(source)) > 0 then return print("cancel") end --will be fired by vorpcharacter:reloadedskinlistener
+    if table.count(jo.framework:getUserIdentifiers(source)) > 0 then return end --will be fired by vorpcharacter:reloadedskinlistener
   end
-  log("=>", jo.framework:getUserIdentifiers(source))
+
   ped = ped
   skin = jo.framework:standardizeSkin(UnJson(skin))
   clothes = jo.framework:standardizeClothes(UnJson(clothes))
