02bec6a70fdf3135c72b7e8e9d7857861a2a4ec5
AlexbavGamer <Alexsandrebav@outlook.com>
2025-04-11 18:02:09 -0300

    Bump 2.0.0.beta.4

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 author "JUMP ON studios : https://jumpon-studios.com"
 documentation "https://docs.jumpon-studios.com"
-version "2.0.0.beta.3"
+version "2.0.0.beta.4"
 package_id "1"
 
 fx_version "adamant"
