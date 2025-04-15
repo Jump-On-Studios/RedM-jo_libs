2ec1f654d0dda946526da272cbd95d00d0d4bbc9
KadDarem <cressent.h@orange.fr>
2025-04-15 13:26:22 +0200

    Bump

--- jo_libs/fxmanifest.lua
+++ jo_libs/fxmanifest.lua
@@ -1,6 +1,6 @@
 author "JUMP ON studios : https://jumpon-studios.com"
 documentation "https://docs.jumpon-studios.com"
-version "2.0.0.beta.5"
+version "2.0.0.beta.6"
 package_id "1"
 
 fx_version "adamant"
