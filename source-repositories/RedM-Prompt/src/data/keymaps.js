76e9d924721bee81526f7d0dde45c2a969e9f227
Brice Nuzzo JO <brice.nuzzo@jumpon-studios.com>
2025-04-18 17:45:59 +0200

    add icons to specific keys

--- source-repositories/RedM-Prompt/src/data/keymaps.js
+++ source-repositories/RedM-Prompt/src/data/keymaps.js
@@ -1,8 +1,47 @@
 export const keymaps = {
-  "LCONTROL": {
-    text: "CTRL"
+  LCONTROL: {
+    text: 'CTRL',
   },
-  "SPACE": {
-    image: "./assets/images/keys/SPACE.png"
-  }
+  SPACE: {
+    image: './assets/images/keys/SPACE.png',
+  },
+  ENTER: {
+    image: './assets/images/keys/ENTER.png',
+  },
+  RETURN: {
+    image: './assets/images/keys/ENTER.png',
+  },
+  PAGEUP: {
+    image: './assets/images/keys/PAGE_UP.png',
+  },
+  PRIOR: {
+    image: './assets/images/keys/PAGE_UP.png',
+  },
+  PAGEDOWN: {
+    image: './assets/images/keys/PAGE_DOWN.png',
+  },
+  NEXT: {
+    image: './assets/images/keys/PAGE_DOWN.png',
+  },
+  HOME: {
+    image: './assets/images/keys/HOME.png',
+  },
+  BACKSPACE: {
+    image: './assets/images/keys/BACKSPACE.png',
+  },
+  BACK: {
+    image: './assets/images/keys/BACKSPACE.png',
+  },
+  LEFT: {
+    image: './assets/images/keys/ARROW_LEFT.png',
+  },
+  UP: {
+    image: './assets/images/keys/ARROW_TOP.png',
+  },
+  RIGHT: {
+    image: './assets/images/keys/ARROW_RIGHT.png',
+  },
+  DOWN: {
+    image: './assets/images/keys/ARROW_BOTTOM.png',
+  },
 }
