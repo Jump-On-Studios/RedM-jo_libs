286c74c7d3eb63cd6c317566da906b12e6146879
KadDarem <cressent.h@orange.fr>
2025-05-09 23:19:25 +0200

    Base project

---
+++ source-repositories/Input/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper" @click="click">
+    Hi !
+  </div>
+</template>
+
+<script setup>
+import Swal from 'sweetalert2'
+
+function click() {
+  Swal.fire({
+    theme: 'dark',
+    title: 'confirmSaveCitizen',
+    showCancelButton: true,
+    confirmButtonText: 'save',
+    confirmButtonColor: 'var(--color-green)',
+  })
+}
+</script>
