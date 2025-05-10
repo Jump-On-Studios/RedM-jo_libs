38a7f84c67bf8340b2d6985104d0ec4a2718dbed
KadDarem <cressent.h@orange.fr>
2025-05-10 22:59:26 +0200

    Remove sweetalert

--- source-repositories/Input/src/App.vue
+++ source-repositories/Input/src/App.vue
@@ -1,19 +1,46 @@
 <template>
-  <div class="wrapper" @click="click">
-    Hi !
+  <div class="notif">
+    <div class="container">
+      <h2 class="title">Title</h2>
+      <div class="actions">
+        <div class="action">
+          Confirm
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import Swal from 'sweetalert2'
+import { ref } from 'vue'
 
-function click() {
-  Swal.fire({
-    theme: 'dark',
-    title: 'confirmSaveCitizen',
-    showCancelButton: true,
-    confirmButtonText: 'save',
-    confirmButtonColor: 'var(--color-green)',
-  })
-}
+const notif = ref({})
 </script>
+
+<style lang="scss" scoped>
+.notif {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  filter: backdrop(4px);
+  background-color: rgba(0, 0, 0, 0.5);
+  font-family: Hapna;
+  color: white;
+}
+
+.container {
+  text-align: center;
+  background-color: var(--color-background-dark);
+  width: 55vw;
+  height: fit-content;
+  margin-top: 25vh;
+  padding: 0.5vw 0.5vw;
+}
+
+.title {
+  font-weight: bold;
+  background-color: var(--color-background-grey);
+  width: 100%;
+}
+</style>
