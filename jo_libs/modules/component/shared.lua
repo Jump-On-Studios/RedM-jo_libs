9a46616f8e312745361b53b4eb1262a03d0359af
KadDarem <cressent.h@orange.fr>
2025-05-19 18:55:03 +0200

    Fix component

--- jo_libs/modules/component/shared.lua
+++ jo_libs/modules/component/shared.lua
@@ -2,14 +2,12 @@ jo.require("table")
 
 jo.component = {}
 
-local function isValidValue(value)
-  return value and value ~= 0 and value ~= -1 and value ~= 1
-end
-
 --- A function to format component data
 ---@param _data string|number|table (The component data)
+---@param hashData boolean (Hash the value is true)
 ---@return any data (The foormatted table for component data)
-function jo.component.formatComponentData(_data)
+function jo.component.formatComponentData(_data, hashData)
+  hashData = GetValue(hashData, false)
   local data = table.copy(_data)
   if type(data) ~= "table" then
     data = { hash = data }
@@ -28,17 +26,12 @@ function jo.component.formatComponentData(_data)
     }
   end
   if type(data.hash) == "table" then data = data.hash end --for VORP
-  if data.hash == 0 or data.hash == false then
-    data.remove = true
-  end
-  data.hash = isValidValue(data.hash) and data.hash or nil
-  data.drawable = isValidValue(data.drawable) and data.drawable or nil
-  data.palette = isValidValue(data.palette) and data.palette or nil
 
-  if not data.hash and not data.drawable and not data.palette and not data.remove then
-    return false
+  if hashData then
+    for key, value in pairs(data) do
+      data[key] = GetHashFromString(value)
+    end
   end
+
   return data
 end
-
-TriggerEvent("print", GetHashFromString("HORSEFEATHERS"))
