86169177e686acc2804ae65ea760ac7c9570fd0c
JOBrice <brice.nuzzo@jumpon-studios.com>
2025-10-27 13:47:27 +0100

    Add Clipboard Module
    * init clipboard module
    
    * init clipboard module
    
    * init clipboard module
    
    * init clipboard module
    
    * add doc
    
    ---------
    
    Co-authored-by: Brice Nuzzo <brice.nuzzo@gmail.com>

---
+++ jo_libs/modules/clipboard/client.lua
@@ -0,0 +1,8 @@
+jo.clipboard = {}
+
+
+--- Copy a string to the user clipboard
+---@param value string (The string to copy)
+jo.clipboard.copy = function(value)
+    return exports.jo_libs:clipboard_copy(value)
+end
