29648b4f8e9f513e8cf92a398d897037820b5a05
KadDarem <cressent.h@orange.fr>
2025-11-06 18:05:52 +0100

    Update menu docs

--- source-repositories/Menu/src/dev.js
+++ source-repositories/Menu/src/dev.js
@@ -6,87 +6,96 @@ let menu = {
   id: 'home',
   title: 'Main title',
   type: 'tile',
-  numberOnLine: 4,
-  numberLineOnScreen: 2,
   subtitle: 'The menu title',
-  numberOnScreen: 3,
-  numberLineOnScreen: 1,
   items: [
     {
       title: 'This is the title',
       icon: "female",
-      iconSize: "small",
-      priceTitle: "Price",
+      // iconSize: "small",
+      // priceTitle: "Price",
       previewSlider: true,
-      price:
-        [{ item: "horseLicense", keep: true }],
+      price: false,
+      // price: [{ item: "horseLicense", keep: true }],
       // footer: "The footer",
       child: "test",
       quantity: 5,
       quantityCircleClass: 'fgold',
       iconClass: 'fgold',
-      sliders: [
-        // {
-        //   type: "switch",
-        //   values: [{ label: "good" }, { label: "bad" }]
-        // },
-        // {
-        //   type: 'grid',
-        //   values: [{ current: 1, min: -10, max: 10 }]
-        // },
-        {
-          type: 'sprite',
-          forceDisplay: true,
-          values: [
-            { palette: { tint0: 1, tint1: 20, tint2: 30, palette: 'tint_generic_clean' }, icon: 'lock', iconClass: 'fred' },
-            { palette: { tint0: 1, tint1: 20, palette: 'tint_generic_clean' }, icon: 'star', iconClass: 'fgold' },
-            { rgb: 'red', icon: 'star', iconClass: 'fgold' },
-            { rgb: ['red', 'blue', "green"], icon: 'star', iconClass: 'fgold' },
-            { sprite: 'tints/metal_engraving_2', icon: 'star', iconClass: 'fgold' }
-          ],
-          description: 'My item description<br>Second line',
-        },
-        // {
-        //   type: 'sprite',
-        //   forceDisplay: true,
-        //   values: [
-        //     { palette: { tint0: 1, tint1: 20, tint2: 30, palette: 'tint_generic_clean' } },
-        //     { palette: { tint0: 1, tint1: 20, palette: 'tint_generic_clean' } },
-        //     { rgb: 'red' },
-        //     { rgb: ['red', 'blue', "green"] },
-        //     { sprite: 'tints/metal_engraving_2' },
-        //     { sprite: 'tints/metal_engraving_2' },
-        //     { sprite: 'tints/metal_engraving_2' },
-        //     { sprite: 'tints/metal_engraving_2' },
-        //     { sprite: 'tints/metal_engraving_2' },
-        //     { sprite: 'tints/metal_engraving_2' },
-        //     { sprite: 'tints/metal_engraving_2' },
-        //     { sprite: 'tints/metal_engraving_2' },
-        //     { sprite: 'tints/metal_engraving_2' },
-        //     { sprite: 'tints/metal_engraving_2' },
-        //     { sprite: 'tints/metal_engraving_2' },
-        //     { sprite: 'tints/metal_engraving_2' },
-        //   ],
-        //   description: 'My item description<br>Second line',
-        // }
-      ]
+      // sliders: [
+      //   // {
+      //   //   type: "switch",
+      //   //   values: [{ label: "good" }, { label: "bad" }]
+      //   // },
+      //   // {
+      //   //   type: 'grid',
+      //   //   values: [{ current: 1, min: -10, max: 10 }]
+      //   // },
+      //   {
+      //     type: 'sprite',
+      //     forceDisplay: true,
+      //     values: [
+      //       { palette: { tint0: 1, tint1: 20, tint2: 30, palette: 'tint_generic_clean' }, icon: 'lock', iconClass: 'fred' },
+      //       { palette: { tint0: 1, tint1: 20, palette: 'tint_generic_clean' }, icon: 'star', iconClass: 'fgold' },
+      //       { rgb: 'red', icon: 'star', iconClass: 'fgold' },
+      //       { rgb: ['red', 'blue', "green"], icon: 'star', iconClass: 'fgold' },
+      //       { sprite: 'tints/metal_engraving_2', icon: 'star', iconClass: 'fgold' }
+      //     ],
+      //     description: 'My item description<br>Second line',
+      //   },
+      //   // {
+      //   //   type: 'sprite',
+      //   //   forceDisplay: true,
+      //   //   values: [
+      //   //     { palette: { tint0: 1, tint1: 20, tint2: 30, palette: 'tint_generic_clean' } },
+      //   //     { palette: { tint0: 1, tint1: 20, palette: 'tint_generic_clean' } },
+      //   //     { rgb: 'red' },
+      //   //     { rgb: ['red', 'blue', "green"] },
+      //   //     { sprite: 'tints/metal_engraving_2' },
+      //   //     { sprite: 'tints/metal_engraving_2' },
+      //   //     { sprite: 'tints/metal_engraving_2' },
+      //   //     { sprite: 'tints/metal_engraving_2' },
+      //   //     { sprite: 'tints/metal_engraving_2' },
+      //   //     { sprite: 'tints/metal_engraving_2' },
+      //   //     { sprite: 'tints/metal_engraving_2' },
+      //   //     { sprite: 'tints/metal_engraving_2' },
+      //   //     { sprite: 'tints/metal_engraving_2' },
+      //   //     { sprite: 'tints/metal_engraving_2' },
+      //   //     { sprite: 'tints/metal_engraving_2' },
+      //   //     { sprite: 'tints/metal_engraving_2' },
+      //   //   ],
+      //   //   description: 'My item description<br>Second line',
+      //   // }
+      // ]
     }, {
       title: 'This is the title',
+      icon: 'male',
       price: 4,
       description: "test",
       textRight: "The text right"
     },
     {
+      icon: "outfit",
       title: 'This is the title',
-      price: { money: 0 },
     },
     {
+      icon: "pants",
       title: 'This is the title',
-      price: 0,
     },
     {
+      icon: "vests",
       title: 'This is the title',
-      price: { gold: 4, money: 4 },
+    },
+    {
+      icon: "hats",
+      title: 'This is the title',
+    },
+    {
+      icon: "eyewear",
+      title: 'This is the title',
+    },
+    {
+      icon: "boots",
+      title: 'This is the title',
     }
   ]
 }
