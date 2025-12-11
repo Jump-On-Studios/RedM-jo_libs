8c9aff84772660723b34d71f0879e612ce7b80a3
Brice Nuzzo <brice.nuzzo@gmail.com>
2025-12-11 17:57:29 +0100

    Update (Menu/NUI) : Add sprite.tagColor, sprite.tagText, sprite.tagTextColor

--- source-repositories/Menu/src/components/layout/SpriteBox.vue
+++ source-repositories/Menu/src/components/layout/SpriteBox.vue
@@ -1,18 +1,38 @@
 <template>
   <div class="sprite-box">
-    <img v-if="sprite.sprite" :class="sprite.class" :src="API.isNUIImage(sprite.sprite) ? sprite.sprite : `./assets/images/${sprite.sprite}.png`" />
+    <div
+      class="tag"
+      v-if="sprite.tagColor || sprite.tagText"
+      :style="{
+        backgroundColor: sprite.tagColor || 'white',
+        color: sprite.tagTextColor || 'black',
+      }"
+    >
+      {{ sprite.tagText }}
+    </div>
+    <img
+      v-if="sprite.sprite"
+      :class="sprite.class"
+      :src="
+        API.isNUIImage(sprite.sprite)
+          ? sprite.sprite
+          : `./assets/images/${sprite.sprite}.png`
+      "
+    />
     <ColorPaletteBox v-else-if="sprite.rgb" :color="sprite" />
     <ColorPaletteBox v-else-if="sprite.palette" :color="sprite.palette" />
     <ColorPaletteBox v-else :color="sprite" />
-    <div class="icon" v-if="sprite.icon" :class="sprite.iconClass"><img :src="API.getImage(sprite.icon)"></div>
+    <div class="icon" v-if="sprite.icon" :class="sprite.iconClass">
+      <img :src="API.getImage(sprite.icon)" />
+    </div>
   </div>
 </template>
 
 <script setup>
-import { inject } from 'vue';
-const props = defineProps(['sprite'])
-const API = inject('API')
-import ColorPaletteBox from './ColorPaletteBox.vue';
+import { inject } from "vue";
+const props = defineProps(["sprite"]);
+const API = inject("API");
+import ColorPaletteBox from "./ColorPaletteBox.vue";
 </script>
 
 <style scoped lang="scss">
@@ -20,6 +40,25 @@ import ColorPaletteBox from './ColorPaletteBox.vue';
   position: relative;
   height: 100%;
   width: 100%;
+}
+
+.tag {
+  position: absolute;
+
+  top: -1vh;
+  right: -1vh;
+  border: 0.25vh solid black;
+  border-radius: 50%;
+  width: 2vh;
+  height: 2vh;
+  font-size: 1vh;
+  color: black;
+  z-index: 10000;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  color: white;
 }
 
 .icon {
--- source-repositories/Menu/src/dev.js
+++ source-repositories/Menu/src/dev.js
@@ -1,15 +1,15 @@
-import PreviewSlider from "./components/layout/PreviewSlider.vue"
+import PreviewSlider from "./components/layout/PreviewSlider.vue";
 
-export default {}
+export default {};
 
 let menu = {
-  id: 'home',
-  title: 'Main title',
-  type: 'tile',
-  subtitle: 'The menu title',
+  id: "home",
+  title: "Main title",
+  type: "tile",
+  subtitle: "The menu title",
   items: [
     {
-      title: 'This is the title',
+      title: "This is the title",
       icon: "female",
       // iconSize: "small",
       // priceTitle: "Price",
@@ -19,108 +19,133 @@ let menu = {
       // footer: "The footer",
       child: "test",
       quantity: 5,
-      quantityCircleClass: 'fgold',
-      iconClass: 'fgold',
-      // sliders: [
-      //   // {
-      //   //   type: "switch",
-      //   //   values: [{ label: "good" }, { label: "bad" }]
-      //   // },
-      //   // {
-      //   //   type: 'grid',
-      //   //   values: [{ current: 1, min: -10, max: 10 }]
-      //   // },
-      //   {
-      //     type: 'sprite',
-      //     forceDisplay: true,
-      //     values: [
-      //       { palette: { tint0: 1, tint1: 20, tint2: 30, palette: 'tint_generic_clean' }, icon: 'lock', iconClass: 'fred' },
-      //       { palette: { tint0: 1, tint1: 20, palette: 'tint_generic_clean' }, icon: 'star', iconClass: 'fgold' },
-      //       { rgb: 'red', icon: 'star', iconClass: 'fgold' },
-      //       { rgb: ['red', 'blue', "green"], icon: 'star', iconClass: 'fgold' },
-      //       { sprite: 'tints/metal_engraving_2', icon: 'star', iconClass: 'fgold' }
-      //     ],
-      //     description: 'My item description<br>Second line',
-      //   },
-      //   // {
-      //   //   type: 'sprite',
-      //   //   forceDisplay: true,
-      //   //   values: [
-      //   //     { palette: { tint0: 1, tint1: 20, tint2: 30, palette: 'tint_generic_clean' } },
-      //   //     { palette: { tint0: 1, tint1: 20, palette: 'tint_generic_clean' } },
-      //   //     { rgb: 'red' },
-      //   //     { rgb: ['red', 'blue', "green"] },
-      //   //     { sprite: 'tints/metal_engraving_2' },
-      //   //     { sprite: 'tints/metal_engraving_2' },
-      //   //     { sprite: 'tints/metal_engraving_2' },
-      //   //     { sprite: 'tints/metal_engraving_2' },
-      //   //     { sprite: 'tints/metal_engraving_2' },
-      //   //     { sprite: 'tints/metal_engraving_2' },
-      //   //     { sprite: 'tints/metal_engraving_2' },
-      //   //     { sprite: 'tints/metal_engraving_2' },
-      //   //     { sprite: 'tints/metal_engraving_2' },
-      //   //     { sprite: 'tints/metal_engraving_2' },
-      //   //     { sprite: 'tints/metal_engraving_2' },
-      //   //     { sprite: 'tints/metal_engraving_2' },
-      //   //   ],
-      //   //   description: 'My item description<br>Second line',
-      //   // }
-      // ]
-    }, {
-      title: 'This is the title',
-      icon: 'male',
+      quantityCircleClass: "fgold",
+      iconClass: "fgold",
+      sliders: [
+        // {
+        //   type: "switch",
+        //   values: [{ label: "good" }, { label: "bad" }]
+        // },
+        // {
+        //   type: 'grid',
+        //   values: [{ current: 1, min: -10, max: 10 }]
+        // },
+        {
+          type: "sprite",
+          forceDisplay: true,
+          values: [
+            {
+              palette: {
+                tint0: 1,
+                tint1: 20,
+                tint2: 30,
+                palette: "tint_generic_clean",
+              },
+              icon: "lock",
+              iconClass: "fred",
+              tagColor: "green",
+              tagText: "1x",
+            },
+            {
+              palette: { tint0: 1, tint1: 20, palette: "tint_generic_clean" },
+              icon: "star",
+              iconClass: "fgold",
+            },
+            { rgb: "red", icon: "star", iconClass: "fgold" },
+            {
+              rgb: ["red", "blue", "green"],
+              icon: "star",
+              iconClass: "fgold",
+              tagColor: "gold",
+            },
+            {
+              sprite: "tints/metal_engraving_2",
+              icon: "star",
+              iconClass: "fgold",
+            },
+          ],
+          description: "My item description<br>Second line",
+        },
+        // {
+        //   type: 'sprite',
+        //   forceDisplay: true,
+        //   values: [
+        //     { palette: { tint0: 1, tint1: 20, tint2: 30, palette: 'tint_generic_clean' } },
+        //     { palette: { tint0: 1, tint1: 20, palette: 'tint_generic_clean' } },
+        //     { rgb: 'red' },
+        //     { rgb: ['red', 'blue', "green"] },
+        //     { sprite: 'tints/metal_engraving_2' },
+        //     { sprite: 'tints/metal_engraving_2' },
+        //     { sprite: 'tints/metal_engraving_2' },
+        //     { sprite: 'tints/metal_engraving_2' },
+        //     { sprite: 'tints/metal_engraving_2' },
+        //     { sprite: 'tints/metal_engraving_2' },
+        //     { sprite: 'tints/metal_engraving_2' },
+        //     { sprite: 'tints/metal_engraving_2' },
+        //     { sprite: 'tints/metal_engraving_2' },
+        //     { sprite: 'tints/metal_engraving_2' },
+        //     { sprite: 'tints/metal_engraving_2' },
+        //     { sprite: 'tints/metal_engraving_2' },
+        //   ],
+        //   description: 'My item description<br>Second line',
+        // }
+      ],
+    },
+    {
+      title: "This is the title",
+      icon: "male",
       price: 4,
       description: "test",
-      textRight: "The text right"
+      textRight: "The text right",
     },
     {
       icon: "outfit",
-      title: 'This is the title',
+      title: "This is the title",
     },
     {
       icon: "pants",
-      title: 'This is the title',
+      title: "This is the title",
     },
     {
       icon: "vests",
-      title: 'This is the title',
+      title: "This is the title",
     },
     {
       icon: "hats",
-      title: 'This is the title',
+      title: "This is the title",
     },
     {
       icon: "eyewear",
-      title: 'This is the title',
+      title: "This is the title",
     },
     {
       icon: "boots",
-      title: 'This is the title',
-    }
-  ]
-}
+      title: "This is the title",
+    },
+  ],
+};
 
 if (import.meta.env.DEV) {
   window.postMessage({
-    event: 'updateLang',
-  })
+    event: "updateLang",
+  });
 
   window.postMessage({
-    event: 'updateMenu',
-    menu: menu
-  })
+    event: "updateMenu",
+    menu: menu,
+  });
 
   let newValues = [
     {
       keys: ["items", 1],
-      action: "delete"
+      action: "delete",
     },
     {
       keys: ["currentIndex"],
       action: "update",
-      value: 1
-    }
-  ]
+      value: 1,
+    },
+  ];
 
   // setTimeout(() => {
   //   window.postMessage({
@@ -137,17 +162,16 @@ if (import.meta.env.DEV) {
   // })
 
   setTimeout(function () {
-
     window.postMessage({
-      event: 'setCurrentMenu',
-      menu: 'home',
-      keepHistoric: true
-    })
+      event: "setCurrentMenu",
+      menu: "home",
+      keepHistoric: true,
+    });
     window.postMessage({
       event: "updateShow",
-      show: true
-    })
-  }, 200)
+      show: true,
+    });
+  }, 200);
 
   // setTimeout(() => {
   //   window.postMessage({
