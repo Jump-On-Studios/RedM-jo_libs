2bf7591eed6ec922d425553af9ca7eed8ea783d8
TitansProductions <84135181+TitansProductions@users.noreply.github.com>
2025-12-14 22:18:33 +0200

    Update client.lua

--- jo_libs/modules/framework-bridge/tpzcore/client.lua
+++ jo_libs/modules/framework-bridge/tpzcore/client.lua
@@ -1,4 +1,6 @@
 
+-- The specified event is triggered once a secondary inventory storage is closed and when it does
+-- we close the saddlebag properly, otherwise animation will be still looped and the saddlebag state will remain active.
 AddEventHandler("tpz_inventory:setSecondaryInventoryOpenState", function(cb)
 
   if cb ~= false then return end
