6095bd46fe2cc8215d463d5375b67ab94e4e3ea1
KadDarem <17074621+KadDarem@users.noreply.github.com>
2026-02-04 14:39:08 +0100

    fix (framework/RSG): load of player clothes without skin data

--- jo_libs/modules/framework-bridge/rsg_2/g_client.lua
+++ jo_libs/modules/framework-bridge/rsg_2/g_client.lua
@@ -1,4 +1,10 @@
 RegisterNetEvent("rsg-appearance:client:ApplyClothes", function(clothes, ped, skin)
   ped = ped or PlayerPedId()
+  if skin and not clothes then
+    return TriggerServerEvent("jo_libs:server:applySkin", ped, skin)
+  end
+  if clothes and not skin then
+    return TriggerServerEvent("jo_libs:server:applyClothes", ped, clothes)
+  end
   TriggerServerEvent("jo_libs:server:applySkinAndClothes", ped, skin, clothes)
 end)
