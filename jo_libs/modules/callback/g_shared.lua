eddcd90a4fd3ace8bc8d220eb76c9e838d7bd881
Brice Nuzzo <brice.nuzzo@gmail.com>
2026-04-30 17:02:26 +0200

    update(callback) : prevent removing jo_libs registered callbacks

--- jo_libs/modules/callback/g_shared.lua
+++ jo_libs/modules/callback/g_shared.lua
@@ -1,3 +1,8 @@
 function generateEventName(action, requestId)
     return "jo_callback:" .. action .. ":" .. requestId
 end
+
+function jo.callback.trigger(name, cb, latent)
+    local func = jo.isClientSide() and jo.callback.triggerClient or jo.callback.triggerServer
+    func(name, cb, latent)
+end
