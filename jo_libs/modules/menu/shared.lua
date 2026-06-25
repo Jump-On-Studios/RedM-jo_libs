459a7cd62798dda991df1e2141b0ff43495dd88f
Brice Nuzzo <brice.nuzzo@gmail.com>
2026-06-25 15:15:16 +0200

    Add ignore annotations for pricing functions in menu module

--- jo_libs/modules/menu/shared.lua
+++ jo_libs/modules/menu/shared.lua
@@ -1,7 +1,11 @@
 jo.createModule("menu")
 jo.require("pricing")
 
+---@ignore
 jo.menu.formatPrice = jo.pricing.formatPrice
+---@ignore
 jo.menu.formatPrices = jo.pricing.formatPrices
+---@ignore
 jo.menu.isPriceFree = jo.pricing.isPriceFree
+---@ignore
 jo.menu.mergePrices = jo.pricing.mergePrices
