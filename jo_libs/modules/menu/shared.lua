bac69eb1cd0b5d650dcdf670f5a02a44975af7de
Brice Nuzzo <brice.nuzzo@gmail.com>
2026-07-02 09:24:10 +0200

    wip retrocompat

--- jo_libs/modules/menu/shared.lua
+++ jo_libs/modules/menu/shared.lua
@@ -1,13 +1,69 @@
 jo.createModule("menu")
 jo.require("pricing")
 
+local PriceClass = jo.pricing.PriceClass
+local PriceGroupClass = jo.pricing.PriceGroupClass
+
 ---@ignore
-jo.menu.formatPrice = jo.pricing.formatPrice
+function jo.menu.formatPrice(price)
+  if price == nil then price = 0 end
+
+  return PriceClass.new(price):get()
+end
+
 ---@ignore
-jo.menu.formatPrices = jo.pricing.formatPrices
+function jo.menu.formatPrices(prices)
+  if prices == nil then
+    return {
+      operator = "and",
+      jo.menu.formatPrice(0)
+    }
+  end
+
+  if type(prices) ~= "table" then
+    return {
+      operator = "and",
+      jo.menu.formatPrice(prices)
+    }
+  end
+
+  if getmetatable(prices) == PriceGroupClass or prices.operator == "or" then
+    local group = PriceGroupClass.new(prices)
+    local formattedPrices = {
+      operator = group.operator
+    }
+
+    for i = 1, #group.prices do
+      formattedPrices[i] = group.prices[i]:get()
+    end
+
+    return formattedPrices
+  end
+
+  return {
+    operator = "and",
+    jo.menu.formatPrice(prices)
+  }
+end
+
 ---@ignore
-jo.menu.isPriceFree = jo.pricing.isPriceFree
+function jo.menu.isPriceFree(price)
+  local prices = jo.menu.formatPrices(price)
+
+  return #prices == 1 and PriceClass.new({
+    costs = prices[1]
+  }):isFree()
+end
+
 ---@ignore
-jo.menu.mergePrices = jo.pricing.mergePrices
+function jo.menu.mergePrices(...)
+  local prices = { ... }
+  prices.operator = "and"
+
+  return PriceGroupClass.new(prices):compact():get()
+end
+
 ---@ignore
-jo.menu.tax = jo.pricing.tax
+function jo.menu.tax(price, percentage, roundUpItems)
+  return PriceClass.new(price):tax(percentage, roundUpItems):get()
+end
