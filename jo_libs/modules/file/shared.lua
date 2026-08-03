107746dc3f7e7b2fb729e8308ecfb04c103bec03
KadDarem <cressent.h@orange.fr>
2026-08-03 17:20:24 +0200

    update (file): Added jo.file.loadJson() & jo.file.readJson()

--- jo_libs/modules/file/shared.lua
+++ jo_libs/modules/file/shared.lua
@@ -48,12 +48,7 @@ function jo.file.load(modname)
   return result
 end
 
---- Read a file and return it if it's exist or `false`. The function accept one or two arguments.
---- One argument: the filepath
---- Two argument: the resource AND the filepath
----@param ... string (path of the file)
----@return file|boolean,resource|string,modpath|string
-function jo.file.read(...)
+local function readFile(extension, ...)
   local args = { ... }
   local resource, modpath
   if args[2] then
@@ -61,9 +56,53 @@ function jo.file.read(...)
   else
     resource, modpath = convertModName(args[1])
   end
-  local file = LoadResourceFile(resource, ("%s.lua"):format(modpath))
+  local file = LoadResourceFile(resource, ("%s.%s"):format(modpath, extension))
 
   return file or false, resource, modpath
+end
+
+--- Read a file and return it if it's exist or `false`. The function accept one or two arguments.
+--- One argument: the filepath
+--- Two argument: the resource AND the filepath
+---@param ... string (path of the file)
+---@return file|boolean,resource|string,modpath|string
+function jo.file.read(...)
+  return readFile("lua", ...)
+end
+
+--- Read a JSON file and return its content if it's exist or `false`. The function accept one or two arguments.
+--- One argument: the filepath
+--- Two argument: the resource AND the filepath
+---@param ... string (path of the file, without the `.json` extension)
+---@return file|boolean,resource|string,modpath|string
+function jo.file.readJson(...)
+  return readFile("json", ...)
+end
+
+--- Loads a JSON file and returns it decoded
+---@param ... string (path of the file, without the `.json` extension)
+---@return table|boolean (The decoded content of the file, or `false` if there was an error)
+function jo.file.loadJson(...)
+  local modname = table.concat({ ... }, "/")
+  dprint(modname, "~orange~: Start loading")
+
+  local file = jo.file.readJson(...)
+
+  if not file then
+    print(("\n^1Impossible to load. File doesn't exist: %s.json^0"):format(modname))
+    return false
+  end
+
+  local success, result = pcall(UnJson, file)
+
+  if not success then
+    print(("\n^1Error loading file (%s.json): %s^0"):format(modname, result))
+    return false
+  end
+
+  dprint(modname, "~green~: Loaded")
+
+  return result
 end
 
 --- Checks if a file exists
@@ -72,3 +111,4 @@ end
 function jo.file.isExist(...)
   return jo.file.read(...) and true or false
 end
+
